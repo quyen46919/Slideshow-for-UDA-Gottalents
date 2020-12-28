23f02ffbb8ded09bdc1a1bd9c6c007970e9d8c5d
Châu Quyền Nguyễn <quyenviettam1@gmail.com>
2020-12-28 08:40:11 +0700

    add old file

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+// lấy nội dung
+var x = document.getElementById('nav-top').innerHTML;
+// gán nội dung
+document.getElementsByClassName('video-items').innerHTML = 'TieuDe';
+// lấy bằng code css, tức là css của phần tử đó như thế nào thì nhập như thể ấy
+var z = document.querySelector('.top-nav form');
+// Tương tự như querySelector, nhưng lấy nhiều phần tử
+var o = document.querySelectorAll('.col-lg-12');
+
+    console.log();
+    alert('Nội dung thông báo đặt ở đây');
+// luu y cach dat ten
+// tên biến 
+var bienNayDungDeLuuGiaTriCuaA;
+// tên hằng số
+const MAU_TOi_DA = 100;
+},false)
+document.addEventListener('',function(){},false)
